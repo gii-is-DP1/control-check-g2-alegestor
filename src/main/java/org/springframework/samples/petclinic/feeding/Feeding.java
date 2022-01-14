3bbf57888b8fd0c6e1e4fac34c9a6ec8b2c737c2
alegestor <alegestor@alum.us.es>
2022-01-14 08:49:29 +0100

    Update Feeding.java

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,16 +2,35 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Feeding {
+
+    @Id
     Integer id;
+
+    @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @NotEmpty
     LocalDate startDate;
+
+    @Column(name = "week_duration")
     double weeksDuration;
+    
+    @NotNull
+    @NotEmpty
     Pet pet;   
 }
